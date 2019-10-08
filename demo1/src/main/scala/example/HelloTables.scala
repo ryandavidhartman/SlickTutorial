4fe8521ddf1f2e3d0afac29cff3fb084e66835d4
ryandavidhartman <ryandavidhartman@gmail.com>
2019-10-08 17:19:39 -0400

    using latest h2 profile

--- demo1/src/main/scala/example/HelloTables.scala
+++ demo1/src/main/scala/example/HelloTables.scala
@@ -2,10 +2,10 @@ package example
 
 import java.util.concurrent.Executors
 
-import slick.driver.H2Driver.api._
+import slick.jdbc.H2Profile.api._
 
 import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor}
 import TableQueries._
 
 import scala.util.control.NonFatal
@@ -14,13 +14,14 @@ object HelloTables extends App {
   println("Start HelloTables Example!")
 
   val db = Database.forConfig("h2mem1")
-  implicit val executionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(4))
+  implicit val executionContext: ExecutionContextExecutor =
+    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(4))
   // or more commonly  import scala.concurrent.ExecutionContext.Implicits.global
 
   try {
     val resultsF = for {
       _ <- db.run(Actions.setup())
-      allSuppliers <- db.run(suppliers.result)  //TableQuery.result => Slick's version of select *
+      allSuppliers <- db.run(suppliers.result)  //TableQuery.result => Slick's version of `select *`
     } yield allSuppliers
 
     val results = Await.result(resultsF, 2 seconds)
